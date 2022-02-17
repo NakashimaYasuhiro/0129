039da290ab672a48f58e77cd5a655655c15c9ea8
NakashimaYasuhiro <n09057015617@gmail.com>
2022-02-17 20:31:12 +0900

    '0217'

--- Assets/unity-chan!/Unity-chan! Model/Art/Animations/Animators/Unitychan0117.cs
+++ Assets/unity-chan!/Unity-chan! Model/Art/Animations/Animators/Unitychan0117.cs
@@ -17,7 +17,7 @@ public class Unitychan0117 : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-        bool isRunning=false;
+        bool isRunning= default(bool);
 
         if (Input.GetKey(KeyCode.RightArrow)){
 
